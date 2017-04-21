b25f5b3a6596ce0cdecc4d249521dcce17a33766
Bram Dutch <adutch56@gmail.com>
2017-04-21 03:52:06 +0000

    MULTILAYER PERCEPTRON WORKINGgit add multilayer_perceptron.pygit add multilayer_perceptron.py!

--- src/multilayer_perceptron.py
+++ src/multilayer_perceptron.py
@@ -24,12 +24,9 @@ BATCH_SIZE = 9 #how many data points to run through between each
 '''display_step = 1''' #removed because it made the code more confusing
 
 # Network Parameters
-n_hidden_1 = 9 # number of nodes in 1st layer
-n_hidden_2 = 9 # number of nodes in 2nd layer
-layer_1_subgraphs = 3 # How many fully connected subgraphs in layer 1?
-layer_2_subgraphs = 1 # How many fully connected subgraphs in layer 2?
-#data_size =  window_size #number of features SHOULDN'T BE A CONSTANT
-CLASSES = 3 #possible classifications to choose between
+LAYER_1_SUBGRAPHS = 1 # How many fully connected subgraphs in layer 1?
+LAYER_2_SUBGRAPHS = 1 # How many fully connected subgraphs in layer 2?
+CLASSES = 4 #possible classifications to choose between
 
 
 ####################################################################
@@ -63,6 +60,8 @@ def create_subconnected_layer(x, weights, biases, num_subgraphs):
         
         #create subgraph by multiplying by weights and adding in bias, as you
         #would with a fully-connected layer
+        print("x_slice = ", x_slice.get_shape().as_list())
+        print("weight[", s, "] = ", weights[s].get_shape().as_list())
         subgraph = tf.add(tf.matmul(x_slice, weights[s]), biases[s])
         subgraph = tf.nn.relu(subgraph)
         layer_list.append(subgraph)
@@ -84,13 +83,13 @@ def create_subconnected_layer(x, weights, biases, num_subgraphs):
 #outputs: returns predictions class labels 
 def multilayer_perceptron(x, weights, biases):
     # Hidden layer with RELU activation
-    layer_1 = create_subconnected_layer(x, weights['h1'], biases['b1'], layer_1_subgraphs)
+    layer_1 = create_subconnected_layer(x, weights['h1'], biases['b1'], LAYER_1_SUBGRAPHS)
     print(layer_1.get_shape().as_list())
     print(x.get_shape().as_list())
     
     
     # Hidden layer with RELU activation
-    layer_2 = create_subconnected_layer(layer_1, weights['h2'], biases['b2'], layer_2_subgraphs)
+    layer_2 = create_subconnected_layer(layer_1, weights['h2'], biases['b2'], LAYER_2_SUBGRAPHS)
     print(layer_2.get_shape().as_list())
     # Output layer with linear activation
     out_layer = tf.matmul(layer_2, weights['out']) + biases['out']
@@ -100,45 +99,82 @@ def multilayer_perceptron(x, weights, biases):
 
 
 
-
-#######################################################
-############        MAIN            ###################
-#######################################################
-if __name__ == "__main__":
-    input_file = str(sys.argv[3])
-    label_file = str(sys.argv[4])
-    window_size = int(sys.argv[1])
-    num_examples = int(sys.argv[2])
-    train_mp(window_size, input_file, label_file, num_examples)
-
-
-def train_mp(window_size, input_file, label_file, num_examples):
+################################################################
+############    TRAIN MULTILAYER PERCEPTRON ####################
+################################################################
+#purpose: create a tensorflow model with 2 hidden layers. The topology of 
+#           the layers is defined in the global variabls section at the top
+#           of this file. After creating the model, it will be trained using
+#           the data in input_file and label_file. The model will then be 
+#           saved to out_file.
+#Example of save/restore function can be found here:
+#   http://stackoverflow.com/questions/33759623/tensorflow-how-to-save-restore-a-model
+#inputs:window_size- the length of a side of the window being used to extract
+#               data. The number of features should be window_size^2.
+#               NOTE: MUST BE AN ODD NUMBER
+#       input_file- the file containing the raw data
+#       label_file- the file containing labels. For each window, the center 
+#               number in label_file will be used as the label for the window
+#       num_examples-the number of windows of data to extract from input_file 
+#               and label_file. 
+#               NOTE: IF THIS IS LARGER THAN THE AMOUNT OF AVAILABLE DATA IN THE
+#               FILES PROVIDED, THE PROGRAM WILL CRASH
+#       out_file- location where you want to save your model.
+#outputs:creates a .meta file (out_file.meta) where the model will be stored.
+#       NOTE: MAY ALSO NEED TO OUTPUT THE CONSTANTS SO THAT THE VARIABLES WORK
+#           WHEN THE MODEL IS LOADED
+def train_mp(window_size, input_file, label_file, num_examples, out_file):
+    data_size = window_size*window_size
     # tf Graph input
-    x = tf.placeholder("float", [None, window_size])  #inputs 
+    x = tf.placeholder("float", [None, (data_size)])  #inputs 
     y_ = tf.placeholder("float", [None, CLASSES])   #ground-truth labels
 
 
-    # Create model with subgraphs
-    assert window_size % layer_1_subgraphs == 0
-    assert n_hidden_1 % layer_1_subgraphs == 0
-    assert n_hidden_2 % layer_2_subgraphs == 0
-    assert CLASSES % layer_2_subgraphs == 0
+    #make sure that topology setup will work
+    layer_1_nodes = data_size
+    layer_2_nodes = data_size
+    assert data_size % LAYER_1_SUBGRAPHS == 0
+    assert layer_1_nodes % LAYER_1_SUBGRAPHS == 0
+    assert layer_2_nodes % LAYER_2_SUBGRAPHS == 0
+    assert CLASSES % LAYER_2_SUBGRAPHS == 0
 
 
 
-    #Store layers weight & bias
+    #create variables to store weights and biases
+    #h1, h2, b1, and b2 contain lists of variables to be used in the subconnected 
+    #   layers
+    #h1 and b1 create variables that each correspond to one of the subgraphs of 
+    #   layer 1. There should be (LAYER_1_SUBGRAPHS) different variables created
+    #   in each. Each variable should be named "h1_[#]" or "b1_[#]", where "#"
+    #   is the variable number
+    #h2 and b2 are the same as h1 and b1 except that they apply to the second
+    #   subconnected layer
+    #the out variables control the input into the fully-connected final layer 
+    #   and are named "out_weights" and "out_biases"
+    #NOTE: THE NAMES ARE NECESSARY TO SAVE THE MODEL TO A FILE
     weights = {
-        'h1': [tf.Variable(tf.random_normal([int(window_size/layer_1_subgraphs), int(n_hidden_1/layer_1_subgraphs)])) for s in range(0, layer_1_subgraphs)],
-        'h2': [tf.Variable(tf.random_normal([int(n_hidden_1/layer_2_subgraphs), int(n_hidden_2/layer_2_subgraphs)])) for s in range(0, layer_2_subgraphs)],
-        'out': tf.Variable(tf.random_normal([int(n_hidden_2), int(CLASSES)]))
+        'h1': [tf.Variable(tf.random_normal([int(data_size/LAYER_1_SUBGRAPHS), int(layer_1_nodes/LAYER_1_SUBGRAPHS)]), name=("h1_"+str(s))) for s in range(0, LAYER_1_SUBGRAPHS)],
+        'h2': [tf.Variable(tf.random_normal([int(layer_1_nodes/LAYER_2_SUBGRAPHS), int(layer_2_nodes/LAYER_2_SUBGRAPHS)]), name=("h2_"+str(s))) for s in range(0, LAYER_2_SUBGRAPHS)],
+        'out': tf.Variable(tf.random_normal([int(layer_2_nodes), int(CLASSES)]), name= "out_weights")
     }
     biases = {
-        'b1': [tf.Variable(tf.random_normal([int(n_hidden_1/layer_1_subgraphs)])) for s in range(0, layer_1_subgraphs)],
-        'b2': [tf.Variable(tf.random_normal([int(n_hidden_2/layer_2_subgraphs)])) for s in range(0, layer_2_subgraphs)],
-        'out': tf.Variable(tf.random_normal([int(CLASSES)]))
+        'b1': [tf.Variable(tf.random_normal([int(layer_1_nodes/LAYER_1_SUBGRAPHS)]), name=("b1_"+str(s))) for s in range(0, LAYER_1_SUBGRAPHS)],
+        'b2': [tf.Variable(tf.random_normal([int(layer_2_nodes/LAYER_2_SUBGRAPHS)]), name=("b2_"+str(s))) for s in range(0, LAYER_2_SUBGRAPHS)],
+        'out': tf.Variable(tf.random_normal([int(CLASSES)]), name="out_biases")
     }
 
-
+    #add variables to collection and initialize the saver
+    for s in range(0, LAYER_1_SUBGRAPHS):
+        tf.add_to_collection('vars', ("h1_"+str(s)))
+        tf.add_to_collection('vars', ("b1_"+str(s)))
+    for s in range(0, LAYER_2_SUBGRAPHS):
+        tf.add_to_collection('vars', ("h2_"+str(s)))
+        tf.add_to_collection('vars', ("b2_"+str(s)))
+    tf.add_to_collection('vars', "out_weights")
+    tf.add_to_collection('vars', "out_biases")
+    saver = tf.train.Saver()
+    
+    
     # Construct model
     y = multilayer_perceptron(x, weights, biases)   #y contains the predicted outputs
                                                 #which will be compared to the 
@@ -155,22 +191,18 @@ def train_mp(window_size, input_file, label_file, num_examples):
     generator = preprocessing.preprocess(input_file, label_file, window_size)
     features = []
     labels = []
-    for _ in range(num_examples):
-        curr = next(generator)
-        #need to convert all to int
+    for count, curr in enumerate(generator):
+        if count >= num_examples:
+            break
         curr_features = curr[0]
-        curr_features = list(map(int, curr_features))
-        print(curr_features)
-        print(curr[1])
+        curr_features = list(map(float, curr_features)) 
+        curr_labels = curr[1]
+        curr_labels = list(map(float, curr_labels))
         features.append(curr_features)
-        labels.append(curr[1])
-    
+        labels.append(curr_labels)    
     features = np.asarray(features)
-    labels = np.asarray(labels)  
-    print(features.shape)
-    print(labels.shape)
-    
-    tf.get_shape
+    labels = np.asarray(labels) 
+        
     # Launch the graph
     with tf.Session() as sess:
         sess.run(init)
@@ -196,10 +228,129 @@ def train_mp(window_size, input_file, label_file, num_examples):
                 
         print("Optimization Finished!")
     
+        #print training accuracy
+        curr_loss = sess.run([cost], feed_dict={x:features, y_:labels})[0]
+        print("The training error was", (curr_loss/num_examples))
+        
+        #output to out_file
+        saver.save(sess, out_file)    
+        #output constants?
 
-        # Test model
-        correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-        # Calculate accuracy
-        accuracy = tf.reduce_mean(tf.cast(correct_prediction, "int32"))
-        for j in range(total_batch):
-            print("Accuracy:", accuracy.eval({x: features, y: labels}))
+
+######################################################
+###########     EVALUATE_MP          ##################
+######################################################
+#purpose: tests a model from in_file on test data from input_file and labels_file
+#inputs:window_size- the length of a side of the window being used to extract
+#               data. The number of features should be window_size^2.
+#               NOTE: MUST BE AN ODD NUMBER
+#       input_file- the file containing the raw test data
+#       label_file- the file containing test labels. For each window, the center 
+#               number in label_file will be used as the label for the window
+#       num_examples-the number of windows of data to extract from input_file 
+#               and label_file. 
+#               NOTE: IF THIS IS LARGER THAN THE AMOUNT OF AVAILABLE DATA IN THE
+#               FILES PROVIDED, THE PROGRAM WILL CRASH
+#       in_file- the .meta file to load the model from
+#outputs: prints out the accuracy of the model on the test data
+def evaluate_mp(window_size, input_file, label_file, num_examples, in_file):
+    data_size = window_size*window_size
+    # tf Graph input
+    x = tf.placeholder("float", [None, (data_size)])  #inputs 
+    y_ = tf.placeholder("float", [None, CLASSES])   #ground-truth labels
+
+
+    #make sure that topology setup will work
+    layer_1_nodes = data_size
+    layer_2_nodes = data_size
+    assert data_size % LAYER_1_SUBGRAPHS == 0
+    assert layer_1_nodes % LAYER_1_SUBGRAPHS == 0
+    assert layer_2_nodes % LAYER_2_SUBGRAPHS == 0
+    assert CLASSES % LAYER_2_SUBGRAPHS == 0
+
+
+
+    #create variables to store weights and biases
+    #h1, h2, b1, and b2 contain lists of variables to be used in the subconnected 
+    #   layers
+    #h1 and b1 create variables that each correspond to one of the subgraphs of 
+    #   layer 1. There should be (LAYER_1_SUBGRAPHS) different variables created
+    #   in each. Each variable should be named "h1_[#]" or "b1_[#]", where "#"
+    #   is the variable number
+    #h2 and b2 are the same as h1 and b1 except that they apply to the second
+    #   subconnected layer
+    #the out variables control the input into the fully-connected final layer 
+    #   and are named "out_weights" and "out_biases"
+    #NOTE: THE NAMES ARE NECESSARY TO SAVE THE MODEL TO A FILE
+    weights = {
+        'h1': [tf.Variable(tf.random_normal([int(data_size/LAYER_1_SUBGRAPHS), int(layer_1_nodes/LAYER_1_SUBGRAPHS)]), name=("h1_"+str(s))) for s in range(0, LAYER_1_SUBGRAPHS)],
+        'h2': [tf.Variable(tf.random_normal([int(layer_1_nodes/LAYER_2_SUBGRAPHS), int(layer_2_nodes/LAYER_2_SUBGRAPHS)]), name=("h2_"+str(s))) for s in range(0, LAYER_2_SUBGRAPHS)],
+        'out': tf.Variable(tf.random_normal([int(layer_2_nodes), int(CLASSES)]), name= "out_weights")
+    }
+    biases = {
+        'b1': [tf.Variable(tf.random_normal([int(layer_1_nodes/LAYER_1_SUBGRAPHS)]), name=("b1_"+str(s))) for s in range(0, LAYER_1_SUBGRAPHS)],
+        'b2': [tf.Variable(tf.random_normal([int(layer_2_nodes/LAYER_2_SUBGRAPHS)]), name=("b2_"+str(s))) for s in range(0, LAYER_2_SUBGRAPHS)],
+        'out': tf.Variable(tf.random_normal([int(CLASSES)]), name="out_biases")
+    }
+
+    #add variables to collection and initialize the saver
+    for s in range(0, LAYER_1_SUBGRAPHS):
+        tf.add_to_collection('vars', ("h1_"+str(s)))
+        tf.add_to_collection('vars', ("b1_"+str(s)))
+    for s in range(0, LAYER_2_SUBGRAPHS):
+        tf.add_to_collection('vars', ("h2_"+str(s)))
+        tf.add_to_collection('vars', ("b2_"+str(s)))
+    tf.add_to_collection('vars', "out_weights")
+    tf.add_to_collection('vars', "out_biases")
+    saver = tf.train.Saver()
+    
+    
+    # Construct model
+    y = multilayer_perceptron(x, weights, biases)   #y contains the predicted outputs
+                                                #which will be compared to the 
+                                                #ground-truth, y_
+
+    # Define loss and optimizer
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=y, labels=y_))
+    optimizer = tf.train.AdamOptimizer(learning_rate=LEARNING_RATE).minimize(cost)
+
+    # Initializing the variables
+    init = tf.global_variables_initializer()
+
+    #get generator for features and labels
+    generator = preprocessing.preprocess(input_file, label_file, window_size)
+    features = []
+    labels = []
+    for count, curr in enumerate(generator):
+        if count >= num_examples:
+            break
+        curr_features = curr[0]
+        curr_features = list(map(float, curr_features)) 
+        curr_labels = curr[1]
+        curr_labels = list(map(float, curr_labels))
+        features.append(curr_features)
+        labels.append(curr_labels)    
+    features = np.asarray(features)
+    labels = np.asarray(labels) 
+    
+    with tf.Session() as sess:
+        #load the data from in_file
+        loader = tf.train.import_meta_graph(in_file)
+        loader.restore(sess, tf.train.latest_checkpoint('./'))
+        
+        total_error = sess.run([cost], feed_dict={x:features, y_:labels})[0]
+        print("The test error was", (total_error/num_examples))        
+
+
+#######################################################
+############        MAIN            ###################
+#######################################################
+#purpose: takes command line arguments and calls train_mp with them
+if __name__ == "__main__":
+    input_file = str(sys.argv[2])
+    label_file = str(sys.argv[3])
+    window_size = int(sys.argv[1])
+    num_examples = int(sys.argv[4])
+    out_file = "DEFAULT.meta"    #DEFAULT is used for the commandline
+    #train_mp(window_size, input_file, label_file, num_examples, out_file)
+    evaluate_mp(window_size, input_file, label_file, num_examples, out_file)
